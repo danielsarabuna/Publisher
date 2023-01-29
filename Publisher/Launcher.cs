63890652def4af90065fea0ce862f82006277bbf
daniel sarabuna <danielsarabuna@gmail.com>
2023-01-29 17:31:30 +0200

    v1.0.3 fix

--- Publisher/Launcher.cs
+++ Publisher/Launcher.cs
@@ -7,47 +7,47 @@ namespace Publisher
 {
     public class Launcher : IDisposable
     {
-        private readonly Dictionary<Type, List<Delegate>> _subscribers;
+        private readonly Dictionary<Type, List<Delegate>> subscribers;
 
         public Launcher()
         {
-            _subscribers = new Dictionary<Type, List<Delegate>>();
+            subscribers = new Dictionary<Type, List<Delegate>>();
         }
 
         public void Dispose()
         {
-            _subscribers.Clear();
+            subscribers.Clear();
         }
 
         internal void Publish<T>(object source, T message)
         {
-            if (source == null) return;
-            if (!_subscribers.ContainsKey(typeof(T))) return;
+            if (source == null || message == null) return;
+            if (!subscribers.ContainsKey(typeof(T))) return;
 
-            var delegates = _subscribers[typeof(T)];
+            var delegates = subscribers[typeof(T)];
             if (delegates == null || delegates.Count == 0) return;
             var payload = new Payload<T>(message, source);
             foreach (var handler in delegates.Select
                          (item => item as Action<Payload<T>>))
             {
-                Task.Factory.StartNew(() => handler?.Invoke(payload));
+                handler?.Invoke(payload);
             }
         }
 
         internal void Subscribe<T>(Action<Payload<T>> subscription)
         {
-            var delegates = _subscribers.ContainsKey(typeof(T)) ? _subscribers[typeof(T)] : new List<Delegate>();
+            var delegates = subscribers.ContainsKey(typeof(T)) ? subscribers[typeof(T)] : new List<Delegate>();
             if (!delegates.Contains(subscription)) delegates.Add(subscription);
 
-            _subscribers[typeof(T)] = delegates;
+            subscribers[typeof(T)] = delegates;
         }
 
         internal void Unsubscribe<T>(Action<Payload<T>> subscription)
         {
-            if (!_subscribers.ContainsKey(typeof(T))) return;
-            var delegates = _subscribers[typeof(T)];
+            if (!subscribers.ContainsKey(typeof(T))) return;
+            var delegates = subscribers[typeof(T)];
             if (delegates.Contains(subscription)) delegates.Remove(subscription);
-            if (delegates.Count == 0) _subscribers.Remove(typeof(T));
+            if (delegates.Count == 0) subscribers.Remove(typeof(T));
         }
     }
 }
--- Publisher/Publisher.cs
+++ Publisher/Publisher.cs
@@ -4,26 +4,26 @@ namespace Publisher
 {
     public static class Publisher
     {
-        private static Launcher _launcher;
+        private static Launcher launcher;
 
         static Publisher()
         {
-            _launcher = new Launcher();
+            launcher = new Launcher();
         }
 
         public static void Publish<T>(this IPublisher<T> source, T message)
         {
-            _launcher.Publish(source, message);
+            launcher.Publish(source, message);
         }
 
         public static void Subscribe<T>(this IListener<T> source, Action<Payload<T>> subscription)
         {
-            _launcher.Subscribe(subscription);
+            launcher.Subscribe(subscription);
         }
 
         public static void Unsubscribe<T>(this IListener<T> source, Action<Payload<T>> subscription)
         {
-            _launcher.Unsubscribe(subscription);
+            launcher.Unsubscribe(subscription);
         }
     }
 }
