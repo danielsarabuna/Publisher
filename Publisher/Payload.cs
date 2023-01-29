5ed33b1d4747619e38a73455f7a1ff7d5c5228d8
daniel sarabuna <danielsarabuna@gmail.com>
2023-01-29 16:37:48 +0200

    v1.0.1

--- Publisher/Payload.cs
+++ Publisher/Payload.cs
@@ -4,9 +4,9 @@ namespace Publisher
 {
     public struct Payload<T> where T : struct
     {
-        public object Who { get; }
-        public T What { get; }
-        public DateTime When { get; }
+        public object Who { get; private set; }
+        public T What { get; private set; }
+        public DateTime When { get; private set; }
 
         public Payload(T payload, object source)
         {
